5bcf340c9513da5afb16947e9a7ebabcd1470489
Nikhil M Jeby <32586986+solid-droid@users.noreply.github.com>
2022-08-06 03:07:37 +0530

    Astro setup with capacitor

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.example.app',
-  appName: 'capacitor-solid-template',
+  appName: 'Digital Twin',
   webDir: 'dist',
   bundledWebRuntime: false,
 };
